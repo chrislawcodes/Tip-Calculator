105294c7d661a8b2e976a5250c484864922dad53
chrislawcodes <chrislaw@gmail.com>
2015-08-27 23:28:51 -0700

    Animation working!
    Got the animation to work properly on the fields, fading them out to
    start and fading in when the user starts editing.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Tip-Calculator
 Codepath Practice Exercise
-Hours: 5
+Hours: 15
+
+This is an iOS application for calculting the tip and full amount you need to pay. Unlike other tip applications, this tip application assumes that you do not want to tip on the taxes that you've paid.
+
+
+REQUIRED STORIES
+- User can enter a subtotal from the bill and see the amount that they need to tip and the full amount of the bill.
+- User can change the percentage they want to tip and see that change reflected in the tip and full amount.
+
+OPTIONAL STORIES
+- User enters total (including taxes) from a credit card slip and the tip and full amount are calculated based on the bill BEFORE taxes. This prevents the user from tipping on taxes.
+- User can toggle whether or not the SF Healthy taxes are included in the total amount.
+- Nicer user experiecnce on startup where all the extra information is stripped away and user just types amount.
+- Information and controls fade in once user starts typing.
+
+FUTURE STORIES
+- add an app icon
+- add a splash screen
+- User can decide if they want to just enter a subtotal before taxes.
+
+NOTES
+Took a while to figure out the animations but VERY satisfying once I got it working!
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var tipControl: UISegmentedControl!
@@ -22,30 +25,72 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var surchargeControl: UISwitch!
     
+    @IBOutlet weak var firstView: UIView!
+
+    @IBOutlet weak var secondView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
         beforetaxLabel.text = "0.00"
-    }
+        billField.becomeFirstResponder()
+
+         UIView.animateWithDuration(0.4, animations: {
+            // This causes first view to fade in and second view to fade out
+            self.firstView.alpha = 1
+            self.secondView.alpha = 0
+        })
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     let surchargeRate = 0.04
     let sftaxRate = 0.095
     
+    @IBAction func onEditStart(sender: UITextField) {
+        UIView.animateWithDuration(0.2, delay: 0.1, options: .CurveEaseOut, animations: {
+            var billFieldFrame = self.billField.frame
+            billFieldFrame.origin.y = 150
+
+            
+            self.billField.frame = billFieldFrame
+            }, completion: { finished in
+                println("Moved the Bill Field First!")
+        })
+        
+    }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        
+       
+        UIView.animateWithDuration(0.1, delay: 0, options: .CurveEaseOut, animations: {
+            var billFieldFrame = self.billField.frame
+            billFieldFrame.origin.y = 30
+            
+            self.billField.frame = billFieldFrame
+            
+            }, completion: { finished in
+                
+            })
+            
+        UIView.animateWithDuration(0.5,animations: {
+            // This causes  second view to fade in
+            self.secondView.alpha = 1
+            self.tipControl.alpha = 1
+            println("Faded In")
+            })
+
+    
         var tipPercentages = [0.18,0.2,0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
         var billAmount = billField.text._bridgeToObjectiveC().doubleValue
-
+        
         func surchargeCalc () -> Double {
             
             if surchargeControl.on {
